36d2776da28ec7f9f3d2e82491633d7a6196a925
vkzeus <150108950+vkzeus@users.noreply.github.com>
2025-04-14 14:59:51 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -16,7 +16,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:3001/login", {
+      const response = await axios.post("https://urbanplaybackend.onrender.com/login", {
         username: userName,
         password: password,
       });
